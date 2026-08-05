dda5449c1b56cac42273bf414f8da64bd65f927d
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:34:32 +0530

    Add SumOfDigits class to calculate digit sum

--- week3/3_readonly_static.ts
+++ week3/3_readonly_static.ts
@@ -1,1 +1,20 @@
-
+class SumOfDigits {
+    readonly number: number;
+    static message: string = "Sum of Digits Program";
+    constructor(num: number) {
+        this.number = num;
+    }
+    calculateSum(): number {
+        let sum = 0;
+        let temp = this.number;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+        return sum;
+    }
+}
+let obj = new SumOfDigits(9876);
+console.log(SumOfDigits.message);
+console.log("Number:", obj.number);
+console.log("Sum of Digits:", obj.calculateSum());
