8bdb26a890268fbed93243cac7189f99668534e0
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:14:43 +0530

    Update 1_datatypes.ts

--- week1/1_datatypes.ts
+++ week1/1_datatypes.ts
@@ -1,1 +1,15 @@
-
+// Number
+let age: number = 19;
+// String
+let name: string = "Bhavya";
+// Boolean
+let isStudent: boolean = true;
+// Array
+let marks: number[] = [85, 90, 95];
+// Tuple
+let student: [number, string] = [101, "Bhavya"];
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(marks);
+console.log(student);
