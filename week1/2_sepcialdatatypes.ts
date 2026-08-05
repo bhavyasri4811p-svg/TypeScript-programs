d5f986f75ddb493ba62cc4400c415b90d305a91b
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:26:41 +0530

    Update 2_sepcialdatatypes.ts

--- week1/2_sepcialdatatypes.ts
+++ week1/2_sepcialdatatypes.ts
@@ -1,19 +1,11 @@
-// any
-let value: any = 10;
-console.log(value);
-value = "Hello";
-console.log(value);
-// unknown
-let input: unknown = "TypeScript";
-if (typeof input === "string") {
-    console.log(input.toUpperCase());
+// Special Data Types
+let value: any = 5678;
+console.log("Any:", value);
+let input: unknown = 1234;
+if (typeof input === "number") {
+    console.log("Unknown:", input);
 }
-// void
-function greet(): void {
-    console.log("Welcome!");
+function display(): void {
+    console.log("Calculating Sum of Digits");
 }
-greet();
-// never
-function showError(message: string): never {
-    throw new Error(message);
-}
+display();
