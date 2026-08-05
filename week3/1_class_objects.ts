bbd050e23e73f6c150fe761458cfec7d8416d8ea
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:32:08 +0530

    Update 1_class_objects.ts

--- week3/1_class_objects.ts
+++ week3/1_class_objects.ts
@@ -1,1 +1,18 @@
-
+class SumOfDigits {
+    number: number;
+    constructor(num: number) {
+        this.number = num;
+    }
+    calculateSum(): number {
+        let sum = 0;
+        let temp = this.number;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+        return sum;
+    }
+}
+let obj = new SumOfDigits(12345);
+console.log("Number:", obj.number);
+console.log("Sum of Digits:", obj.calculateSum());
