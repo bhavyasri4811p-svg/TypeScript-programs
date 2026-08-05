67632ba9e422dedf19da20e1cb8eefe044522b72
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:15:50 +0530

    Update 3_annotations-demo.ts

--- week1/3_annotations-demo.ts
+++ week1/3_annotations-demo.ts
@@ -1,1 +1,12 @@
-
+// Variable annotations
+let id: number = 1;
+let username: string = "Bhavya";
+let active: boolean = true;
+// Function annotations
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log(id);
+console.log(username);
+console.log(active);
+console.log(add(10, 20));
