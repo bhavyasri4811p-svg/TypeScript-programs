1608044d81a4d6e49c47ef22aaf45fcb18d3ef9a
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:22:03 +0530

    Update 2_arrow-functions.ts

--- week2/2_arrow-functions.ts
+++ week2/2_arrow-functions.ts
@@ -1,1 +1,13 @@
-
+// Arrow function to add two numbers
+const add = (a: number, b: number): number => {
+    return a + b;
+};
+// Arrow function to find square
+const square = (num: number): number => num * num;
+// Arrow function without parameters
+const welcome = (): void => {
+    console.log("Welcome to Arrow Functions");
+};
+console.log("Addition =", add(15, 5));
+console.log("Square =", square(6));
+welcome();
