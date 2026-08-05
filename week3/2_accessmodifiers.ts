3d6d95c1160cecf1b0bcde2cd99a24a0b13e59fc
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:33:01 +0530

    Add SumOfDigits class to calculate digit sum

--- week3/2_accessmodifiers.ts
+++ week3/2_accessmodifiers.ts
@@ -1,1 +1,17 @@
-
+class SumOfDigits {
+    private number: number;
+    constructor(num: number) {
+        this.number = num;
+    }
+    public calculateSum(): number {
+        let sum = 0;
+        let temp = this.number;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp = Math.floor(temp / 10);
+        }
+        return sum;
+    }
+}
+let obj = new SumOfDigits(5678);
+console.log("Sum of Digits:", obj.calculateSum());
