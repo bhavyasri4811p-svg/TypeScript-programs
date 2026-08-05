41074ec34f8da96edcc0ca4fca3ee85a3e431fa9
bhavyasri4811p-svg <bhavyasri4811.p@gmail.com>
2026-08-05 21:21:29 +0530

    Update 1_functions-demo.ts

--- week2/1_functions-demo.ts
+++ week2/1_functions-demo.ts
@@ -1,1 +1,17 @@
-
+// Function without parameters
+function greet(): void {
+    console.log("Welcome to TypeScript!");
+}
+// Function with parameters
+function add(a: number, b: number): number {
+    return a + b;
+}
+// Function with default parameter
+function display(name: string = "Student"): void {
+    console.log("Hello " + name);
+}
+// Function call
+greet();
+console.log("Sum =", add(10, 20));
+display();
+display("Bhavya");
